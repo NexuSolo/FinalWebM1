644f1f2386cd267c112aa08c3ccf55baba81ed2c
Nicolas Theau <theaun91@gmail.com>
2024-06-19 09:51:21 +0200

    back

--- src/utilisateur/utilisateur.resolver.ts
+++
@@ -1,9 +0,0 @@
-import { Resolver } from '@nestjs/graphql';
-import { Utilisateur } from './models/utilisateur.model';
-
-@Resolver(of => Utilisateur)
-export class UtilisateurResolver {
-
-    
-
-}
