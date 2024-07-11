bebd902e5a0cf144ab3ef9939d4d5be17bb10824
Yanis Rozier <yanis.rozier@orange.fr>
2024-07-11 10:44:57 +0200

    arrangement front

---
+++ front/src/components/BoutonDeco.vue
@@ -0,0 +1,56 @@
+<template>
+    <div v-if="isUserLoggedIn">
+        <button @click="logout">Déconnexion</button>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'BoutonDeco',
+    computed: {
+        isUserLoggedIn() {
+            return localStorage.getItem('userId') !== null;
+        }
+    },
+    methods: {
+        logout() {
+            // Supprimer les données de l'utilisateur du stockage local ou de la session
+            localStorage.clear();
+
+            // Rediriger l'utilisateur vers la page de connexion
+            this.$router.push('/').then(() => {
+                location.reload();
+            });
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+
+button{
+    background-color: #e3e3e3; /* Green */
+    border: none;
+    color: rgb(54, 54, 54);
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+    padding: 10px 24px;
+    border-radius: 12px;
+    transition-duration: 0.4s;
+    position: absolute;
+    right: 10%;
+    top: 93%;
+}
+
+button:hover {
+    background-color: #555555;
+    color: white;
+}
+
+</style>
--- front/src/components/PageAccueil.vue
+++ front/src/components/PageAccueil.vue
@@ -1,32 +1,65 @@
 <template>
   <div class="content">
-    <h1>Accueil</h1>
-    <!-- Bouton de déconnexion -->
-    <button @click="logout">Déconnexion</button>
+    <!-- Rendre le logo cliquable et le lier à l'URL '/conversations' -->
+
+    <div v-if="isUserLoggedIn">
+      <router-link to="/conversations">
+        <img class="logo" src="../assets/mesage.png" alt="">
+      </router-link>
+    </div>
+    <div v-else>
+      <img class="logo" src="../assets/mesage.png" alt="">
+    </div>
+
+    <!-- Afficher le bouton de connexion si l'utilisateur n'est pas connecté -->
+    <div v-if="!isUserLoggedIn" class="button-connection" @click="goToConnection">
+      Se connecter
+    </div>
+    <!-- Afficher le message de bienvenue si l'utilisateur est connecté -->
+    <div v-else>
+      Bonjour {{ userName }}
+    </div>
   </div>
+
+  <BouttonDeco />
 </template>
 
 <script>
+import BouttonDeco from './BoutonDeco.vue';
+
 export default {
+  components: {
+    BouttonDeco
+  },
+  mounted() {
+    console.log(localStorage);
+  },
   name: 'PageAccueil',
   data() {
     return {
       name: ''
     }
   },
-  mounted() {
-    console.log(localStorage);
+  computed: {
+    isUserLoggedIn() {
+      // Vérifie si 'userToken' existe dans le localStorage pour déterminer si l'utilisateur est connecté
+      return localStorage.getItem('userId') !== null;
+    },
+    userName() {
+      // Récupère le nom de l'utilisateur du localStorage
+      return localStorage.getItem('username');
+    }
   },
   methods: {
-    submitForm() {
-      console.log(this.name);
+    goToConnection() {
+      this.$router.push('/connection');
     },
     logout() {
       // Supprimer les données de l'utilisateur du stockage local ou de la session
-      localStorage.clear(); // ou sessionStorage.clear() selon ce que vous utilisez
+      localStorage.clear();
 
       // Rediriger l'utilisateur vers la page de connexion
-      this.$router.push('/connection').then(() => {
+      this.$router.push('/').then(() => {
           location.reload();
         });
     }
@@ -35,4 +68,40 @@ export default {
 </script>
 
 <style scoped>
+
+.button-connection{
+  background-color: #e3e3e3; /* Green */
+  border: none;
+  color: rgb(54, 54, 54);
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+  padding: 10px 24px;
+  border-radius: 12px;
+  transition-duration: 0.4s;
+}
+
+.button-connection:hover{
+  background-color: #d3d3d3;
+}
+
+.content {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+}
+
+.logo{
+  width: 10vh;
+  margin-bottom: 20px;
+  opacity: 0.3;
+}
+
 </style>
